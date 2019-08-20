2406b13cc7dabe9300719967c2bf68b4d370cd16
3103ski <bryan@uppermesa.com>
2019-08-19 20:26:43 -0700

    adds empty reducers for containers

---
+++ src/store/reducers/fileMngrReducer.js
@@ -0,0 +1,12 @@
+import * as actionTypes from '../actions';
+
+const intitialState = {};
+
+const fileMngrReducer = (state = intitialState, action) => {
+	switch (action.type) {
+		default:
+			return state;
+	}
+};
+
+export default fileMngrReducer;
---
+++ src/store/reducers/todoReducer.js
@@ -0,0 +1,56 @@
+import * as actionTypes from '../actions';
+
+const intitialState = {
+	filters: {
+		all: 18,
+		inbox: 1,
+		myDay: 0,
+		today: 0,
+		tomorrow: 4,
+		nextSeven: 13
+	},
+	todos: [
+		{
+			title: 'send final mix',
+			notes: 'I really need to get those files out to whats his name.',
+			dueDate: 'today',
+			myDay: false,
+			location: {
+				artist: 'Jimmy Cones',
+				project: 'Do It Again',
+				song: 'Skyscraper'
+			}
+		},
+		{
+			title: 'new vocal track',
+			notes: 'gotta get that vocal swapped out bruh.',
+			dueDate: 'tomorrow',
+			myDay: false,
+			location: {
+				artist: 'Jimmy Cones',
+				project: 'Do It Again',
+				song: 'Skyscraper'
+			}
+		},
+		{
+			title: 'send stems',
+			notes: 'lets get those stems sent the eff out.',
+			dueDate: '10/12/19',
+			myDay: true,
+			location: {
+				artist: 'Jimmy Cones',
+				project: 'Do It Again',
+				song: 'Skyscraper'
+			}
+		}
+	]
+};
+
+const todoReducer = (state = intitialState, action) => {
+	switch (action.type) {
+		default:
+			return state;
+	}
+};
+
+export default todoReducer;
