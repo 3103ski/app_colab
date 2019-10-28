a3cc2a5d2551053d62d4ecf5ba93f0e397204b9d
3103ski <bryan@uppermesa.com>
2019-10-27 19:59:55 -0700

    adds dragable status cards to dash

--- src/containers/DashboardContainer/Dashboard.js
+++ src/containers/DashboardContainer/Dashboard.js
@@ -1,10 +1,12 @@
 // React Imports
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Board from 'react-trello';
 import * as actions from '../../store/actions/index';
+// import * as actions from '../../store/actions/index';
 
-// Helper functions
-// import genId from '../../components/HelperFunctions/uniqueID';
+// Utility Functions
+import { updateObject } from '../../shared/utility';
 
 // Components
 import DashList from '../../components/DashComponents/DashListContainer/DashListContainer';
@@ -14,8 +16,172 @@ import TodoList from '../../components/TodoComponents/TodoList/TodoList';
 import classes from './Dashboard.module.css';
 
 class Dashboard extends Component {
+	returnCard = (id, title, artist) => {
+		let card = {
+			id: id,
+			title: title,
+			description: `By: ${artist}`,
+			hideCardDeleteIcon: false,
+			draggable: true
+		};
+		return card;
+	};
+	cardChange = (fromLan, toLane, songId, index) => {
+		console.log(`We moved something`, fromLan);
+		console.log(`We moved something`, toLane);
+		console.log(`We moved something`, songId);
+		console.log(`We moved something`, index);
+		let newStatus;
+		if (toLane === 'new_songs_lane') {
+			newStatus = 'New Song';
+		} else if (toLane === 'in_progress_lane') {
+			newStatus = 'In Progress';
+		} else if (toLane === 'mix_sent_lane') {
+			newStatus = 'Mix Sent';
+		} else if (toLane === 'revisions_requested_lane') {
+			newStatus = 'Revisions Requested';
+		} else if (toLane === 'live_stream_scheduled_lane') {
+			newStatus = 'Live Stream Scheduled';
+		} else if (toLane === 'sent_final_mixes_lane') {
+			newStatus = 'Sent Final Mixes';
+		} else if (toLane === 'completed_lane') {
+			newStatus = 'Completed';
+		}
+
+		let song = this.props.songs.filter(song =>
+			song.id === songId ? song : null
+		)[0];
+		let newSong = updateObject(song, {
+			status: newStatus
+		});
+		this.props.updateStatus(newSong, this.props.token);
+		console.log(`::: UPDATE THIS ???,`, newSong);
+	};
 	render() {
-		const todos = [...this.props.todos];
+		let newSongs,
+			nsCards,
+			inProgress,
+			ipCards,
+			mixSent,
+			msCards,
+			revisionsRequested,
+			rrCards,
+			liveStreamScheduled,
+			lsCards,
+			sentFinalMixes,
+			fmCards,
+			completed,
+			comCards;
+
+		// STATUS ARRAYS OF SONGS
+		newSongs = this.props.songs.filter(song =>
+			song.status === `New Song` ? song : null
+		);
+		inProgress = this.props.songs.filter(song =>
+			song.status === `In Progress` ? song : null
+		);
+		mixSent = this.props.songs.filter(song =>
+			song.status === `Mix Sent` ? song : null
+		);
+		revisionsRequested = this.props.songs.filter(song =>
+			song.status === `Revisions Requested` ? song : null
+		);
+		liveStreamScheduled = this.props.songs.filter(song =>
+			song.status === `Live Stream Scheduled` ? song : null
+		);
+		sentFinalMixes = this.props.songs.filter(song =>
+			song.status === `Sent Final Mixes` ? song : null
+		);
+		completed = this.props.songs.filter(song =>
+			song.status === `Completed` ? song : null
+		);
+
+		// ARRAYS OF CARDS OBJECTS MADE OF STATUS ARRAYS
+		nsCards = newSongs.map(song => {
+			return this.returnCard(song.id, song.name, song.artist);
+		});
+		ipCards = inProgress.map(song =>
+			this.returnCard(song.id, song.name, song.artist)
+		);
+		msCards = mixSent.map(song =>
+			this.returnCard(song.id, song.name, song.artist)
+		);
+		rrCards = revisionsRequested.map(song =>
+			this.returnCard(song.id, song.name, song.artist)
+		);
+		lsCards = liveStreamScheduled.map(song =>
+			this.returnCard(song.id, song.name, song.artist)
+		);
+		fmCards = sentFinalMixes.map(song =>
+			this.returnCard(song.id, song.name, song.artist)
+		);
+		comCards = completed.map(song =>
+			this.returnCard(song.id, song.name, song.artist)
+		);
+
+		console.log(`TEST new song cards`, nsCards);
+		console.log(`IN PROGRESS cards`, ipCards);
+		console.log(`MIX SENT cards`, msCards);
+		console.log(`REVISIONS REQUESTED cards`, rrCards);
+		console.log(`LIVE SECHDEULED cards`, lsCards);
+		console.log(`FINAL MIXES cards`, fmCards);
+		console.log(`COMPLETED cards`, comCards);
+		let data = {
+			lanes: [
+				{
+					id: 'new_songs_lane',
+					title: 'New Songs',
+					cards: [...nsCards]
+				},
+				{
+					id: 'in_progress_lane',
+					title: 'In Progress',
+					// label: '0/0',
+					cards: [...ipCards]
+				},
+				{
+					id: 'mix_sent_lane',
+					title: 'Mix Sent',
+					// label: '0/0',
+					cards: [...msCards]
+				},
+				{
+					id: 'revisions_requested_lane',
+					title: 'Revisions Requested',
+					// label: '0/0',
+					cards: [...rrCards]
+				},
+				{
+					id: 'live_stream_scheduled_lane',
+					title: 'Live Stream Scheduled',
+					// label: '0/0',
+					cards: [...lsCards]
+				},
+				{
+					id: 'sent_final_mixes_lane',
+					title: 'Sent Final Mixes',
+					// label: '0/0',
+					cards: [...fmCards]
+				},
+				{
+					id: 'completed_lane',
+					title: 'Completed',
+					// label: '0/0',
+					cards: [...comCards]
+				}
+			]
+		};
+
+		const statusOptions = [
+			`New Song`,
+			`In Progress`,
+			`Mix Sent`,
+			`Revisions Requested`,
+			`Live Stream Scheduled`,
+			`Sent Final Mixes`,
+			`Completed`
+		];
+
 		return (
 			<div className={classes.DashContainer}>
 				<div className={classes.TopDash}>
@@ -106,7 +272,29 @@ class Dashboard extends Component {
 					</DashList>
 				</div>
 				<div className={classes.BottomDash}>
-					<h1>TRELLO-LIKE LISTS GO HERE</h1>
+					{/* <h1>TRELLO-LIKE LISTS GO HERE</h1> */}
+					<Board
+						hideCardDeleteIcon
+						onCardMoveAcrossLanes={this.cardChange}
+						data={data}
+						style={{
+							backgroundColor: 'white',
+							height: 'calc(100vh - 385px)'
+						}}
+						cardStyle={{
+							textAlign: 'left'
+						}}
+						labelStyle={{
+							color: 'red'
+						}}
+						laneStyle={{
+							backgroundColor: 'rgb(45, 156, 219)',
+							boxShadow: '-1px 1px 13px #80808042',
+							transition: '.4s',
+							color: 'white'
+							// backgroundColor: '#2d9cdb'
+						}}
+					/>
 				</div>
 			</div>
 		);
@@ -119,12 +307,16 @@ const mapStateToProps = state => {
 		firstName: state.user.firstName,
 		lastName: state.user.lastName,
 		password: state.user.password,
-		todos: state.todo.todos
+		todos: state.todo.todos,
+		songs: state.projects.songs,
+		token: state.auth.token
 	};
 };
 
 const mapDispatchToProp = dispatch => {
-	return {};
+	return {
+		updateStatus: (song, token) => dispatch(actions.updateSong(song, token))
+	};
 };
 
 export default connect(
